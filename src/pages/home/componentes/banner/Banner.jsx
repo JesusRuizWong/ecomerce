0a15f79f3d60b8580e3aa64d12c83e4cff2467ab
JesusRuizWong <jesusruizwong@outlook.com>
2022-11-21 00:52:44 -0500

    se arreglo el banner

--- src/pages/home/componentes/banner/Banner.jsx
+++ src/pages/home/componentes/banner/Banner.jsx
@@ -29,39 +29,45 @@ export default function Banner() {
  
   return (
     <div>
-         <div id="carouselExampleCaptions" className="carousel slide" data-mdb-ride="carousel" data-mdb-interval="false">
+         <div id="carouselExampleIndicators1" className="carousel slide" data-ride="carousel">
   <div className="carousel-indicators "  >
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"  ></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2" ></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"  ></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="1" aria-label="Slide 2" ></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div className="carousel-inner">
+  
+  <div className="carousel-item active" data-mdb-interval="2000">
    <Slider 
       imagen="1"
       titulo="Primer Titulo"
       descripcion="descripcion 01"
-
    />
+  </div> 
 
-<Slider 
+  <div className="carousel-item " data-mdb-interval="2000">
+  <Slider 
        imagen="2"
        titulo="sEGUNDO  2cTitulo"
        descripcion="descripcion 01"
    
    /> 
+</div> 
+
+<div className="carousel-item " data-mdb-interval="2000">
 <Slider 
        imagen="3"
        titulo="Primer 3232cTitulo"
        descripcion="descripcion 0ddddddddddddd1"
    
    /> 
-
+</div>
   </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev" >
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide="prev" >
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next" id="btn1">
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide="next" id="btn1">
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
--- src/pages/home/componentes/banner/Slider.jsx
+++ src/pages/home/componentes/banner/Slider.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Slider({imagen,titulo,descripcion}) {
   return (
     <div>
-        <div className="carousel-item active" data-mdb-interval="2000">
+
             
             <img id="bannerImg" src={`src/assets/images/banner/slide-${imagen}.jpg`} className="d-block w-100" alt="slider"></img>
             <div className="carousel-caption d-none d-md-block">
                 <h5>{titulo}</h5>
                 <p>{descripcion}</p>
             </div>
-        </div>
+    
 
     </div>
   )
